c2d3bc12c427b365319076c6fcbf902fdd0af880
Vinay Singh <121799304+vinaynit@users.noreply.github.com>
2023-09-23 06:40:15 +0000

    hi this is

--- fisrt.js
+++ fisrt.js
@@ -1,1 +1,13 @@
-console.log("vinay neta nahi ho");
+const accountId=144343
+let accountEmail="vinay@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=2  not allowed
+console.log(accountId);
+
+accountEmail="mona@gmail.com"
+accountPassword="25142"
+accountCity="Rewa"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
